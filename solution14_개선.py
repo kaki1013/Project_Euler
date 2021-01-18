58f5782d143b95cc488814c1c06582fa6477ccde
kaki1013 <65400693+kaki1013@users.noreply.github.com>
2021-01-18 17:01:47 +0900

    Update solution14_개선.py
    리스트 사용하지 않음, 다른 사람 풀이 추가(cache, 캐시)

--- solution14_개선.py
+++ solution14_개선.py
@@ -1,23 +1,43 @@
-# 최대 시행 횟수와 그 수를 기록 후 더 오래 걸리면 교체?
-def collatz(num):
-    if num % 2 == 0:
-        num /= 2
-    else:
-        num = 3 * num + 1
-    return int(num)
-
-
-step_list = []
-
+# 83.87875270843506s
+max_step = 0
+max_index = 0
 for i in range(1, 10 ** 6 + 1):
     step = 0
     num = i
     while not num == 1:
         step += 1
-        num = collatz(num)
-    step_list.append(step)
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = 3 * num + 1
+    if step > max_step:
+        max_step = step
+        max_index = i
+print(max_index)
 
-longest = step_list.index(max(step_list))
-# index 함수는 리스트에 x 값이 있으면 x의 위치 값을 돌려주는 위치반환
 
-print(longest + 1)
+# others1: 3.113677740097046s
+def collatz(n): return n // 2 if n % 2 == 0 else 3 * n + 1
+
+
+def distance(n, cache={1: 1}):
+    if n not in cache: cache[n] = distance(collatz(n)) + 1
+    return cache[n]
+
+
+print(max(range(1, 1000000), key=distance))
+
+
+# others2: 3.403927803039551s
+def longest_collatz_sequence(t):
+    cache = {1: 1}
+
+    def collatz_(n):
+        if n not in cache:
+            cache[n] = collatz_(3 * n + 1 if n % 2 else n / 2) + 1
+        return cache[n]  # Length of Collatz Chain
+
+    return max(range(1, t), key=collatz_)  # Greatest Chain
+
+
+print(longest_collatz_sequence(1000000))
